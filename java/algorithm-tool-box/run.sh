e5c487965311863910ac1692abdaba81c59570fc
Fetswcy <alanzplus@gmail.com>
2016-07-04 13:04:35 +0900

    Clean up

--- java/algorithm-tool-box/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn package && clear && java -jar target/algorithm-tool-box-1.0.0-SNAPSHOT-jar-with-dependencies.jar
--- java/commons/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn package && clear && java -jar target/commons-1.0.0-SNAPSHOT-jar-with-dependencies.jar
